0aae5acbbe19510dc2d4724dbd81f0cd8ed8264d
mimosa23 <dev.00001001@gmail.com>
2022-05-06 23:56:05 +0900

    NGの1ケースが見つかっていない

--- workspace/src/atcoder_problems/abc060/b/main.py
+++ workspace/src/atcoder_problems/abc060/b/main.py
@@ -1,1 +1,20 @@
-n=input()
+a,b,c=map(int,input().split())
+
+yes="YES"
+no="NO"
+if a%2==0 and c%2==1:
+    print(no)
+    exit()
+if a==1:
+    print(yes)
+    exit()
+
+l=[]
+for i in range(1,10**9):
+    if a*i in l:
+        print(no)
+        exit()
+    if a*i%b == c:
+        print(yes)
+        exit()
+    l.append(a*i)
